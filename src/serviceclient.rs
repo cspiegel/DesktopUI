edf29c26a8f7c022e3125f8b2783ff1ead49cd9e
Chris Spiegel <chris.spiegel@zerotier.com>
2025-11-12 09:27:29 -0800

    Mac fix

--- src/serviceclient.rs
+++ src/serviceclient.rs
@@ -16,6 +16,8 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 
+use std::ffi::CString;
+
 use serde_json::{Map, Value};
 use ureq::{Agent, AgentBuilder};
 
